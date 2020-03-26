4b41cbc43890d60d87c41c176bbf54dfa693d554
Adarsh Hegde <adhegde001@gmail.com>
2020-03-26 17:47:45 +0530

    fixed caching sort of

--- routes/routes.ytplayer.js
+++ routes/routes.ytplayer.js
@@ -11,49 +11,57 @@ const hook = (main) => {
     const router = express()
 
     router.post("/:id", async (req, res) => {
-        const url = 'http://www.youtube.com/watch?v=' + req.params.id
-        const {userId, track_id, playlist} = req.body;
-       if(!cache.hasOwnProperty(req.params.id)){
-            youtubedl.getInfo(url, [], function(err, info) {
-            if (err) throw err
-            res.send(info.formats[0].url, null, 1);
-            // cache[req.params.id] = info.formats[0].url;
-            console.log(playlist);
-            if(playlist !== "newlyreleased")
-                LibraryController.incrementPlays(userId, track_id);
-            else 
-              LibraryController.addTrackToLibrary(userId,{
-                name:req.body.track.name,
-                author:req.body.track.author,
-                image:req.body.track.image,
-                source:req.body.track.source,
-                track_id:req.body.track.track_id,
-                plays:1,
-            });
-        });} else {
-            console.log("serving " + req.params.id + " from cache.");
-            res.send(cache[req.params.id]);
-            console.log(playlist);
-            if(playlist !== "newlyreleased")
-                LibraryController.incrementPlays(userId, track_id);
-            else 
-              LibraryController.addTrackToLibrary(userId,{
-                name:req.track.name,
-                author:req.track.author,
-                image:req.track.image,
-                source:req.track.source,
-                track_id:req.track.track_id,
-                plays:1,
-            });
+        const source_id = req.params.id;
+        const url = 'http://www.youtube.com/watch?v=' + source_id
+        const { userId, track_id, playlist } = req.body;
+        let diff = -1;
+        if (cache.hasOwnProperty(source_id)) {
+            let date1 = new Date(cache[source_id].t);
+            let date2 = new Date();
+            diff = Math.abs(date1 - date2) / 36e5;
         }
-          
+        if (!cache.hasOwnProperty(source_id) && (diff == -1 || diff >= 1)) {
+            youtubedl.getInfo(url, [], function (err, info) {
+                if (err) throw err
+                res.send(info.formats[0].url, null, 1);
+                cache[source_id] = { t: Date.now(), url: info.formats[0].url };
+                console.log(playlist);
+                if (playlist !== "newlyreleased")
+                    LibraryController.incrementPlays(userId, track_id);
+                else
+                    LibraryController.addTrackToLibrary(userId, {
+                        name: req.body.track.name,
+                        author: req.body.track.author,
+                        image: req.body.track.image,
+                        source: req.body.track.source,
+                        track_id: req.body.track.track_id,
+                        plays: 1,
+                    });
+            });
+        } else {
+            console.log("serving " + source_id + " from cache." +" " + diff + " hours has passed");
+            res.send(cache[source_id].url);
+
+            if (playlist !== "newlyreleased")
+                LibraryController.incrementPlays(userId, track_id);
+            else
+                LibraryController.addTrackToLibrary(userId, {
+                    name: req.body.track.name,
+                    author: req.body.track.author,
+                    image: req.body.track.image,
+                    source: req.body.track.source,
+                    track_id: req.body.track.track_id,
+                    plays: 1,
+                });
+        }
+
     })
 
     router.get("/info/:id", async (req, res) => {
         const url = 'http://www.youtube.com/watch?v=' + req.params.id
-    
 
-            youtubedl.getInfo(url, [], function(err, info) {
+
+        youtubedl.getInfo(url, [], function (err, info) {
             if (err) throw err
             res.send({
                 thumbnail: info.thumbnail,
@@ -62,9 +70,9 @@ const hook = (main) => {
             });
             // LibraryController.incrementPlays(userId, track_id);
         });
-          
+
     })
-   
+
 
     return router;
 }
